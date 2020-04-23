4f673b1eb94f0ff514b34e53a87695240041d40e
milkzzy <jackmisdom@gmail.com>
2020-04-23 12:50:51 -0400

    Remove Defauls

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './App.css';
+
+const AppHeader = () => {
+    return React.createElement('h1', { className: 'app-header' }, 'Pongboard');
+};
+
+const Game = () => {
+    return (
+        React.createElement('ul', {},
+            React.createElement('li', {}, 'Player 1'),
+            React.createElement('li', {}, 'Player 2'),
+        )
+    )
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    React.createElement('div', {},
+        React.createElement(AppHeader, {}, null),
+        React.createElement(Game, {}, null),
+    ),
+    document.getElementById('root'),
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
